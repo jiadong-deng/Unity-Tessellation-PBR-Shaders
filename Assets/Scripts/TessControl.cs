f6f853160241aa6bb19b04ce1880aaabeaa49254
Maxwell <gengy@msoe.edu>
2018-03-31 16:51:55 -0500

    optimizing

---
+++ Assets/Scripts/TessControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Tessellation control.
+/// tessellation in forward rendering with shadowcaster pass will have terrible performance
+/// So I decide to disable the tessellation in forward rendering mode and you should also inplement this component in each camera.
+/// </summary>
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class TessControl : MonoBehaviour {
+	Camera cam;
+	void Awake(){
+		cam = GetComponent<Camera> ();
+	}
+	/// <summary>
+	/// The keyword ENABLE_TESSELLATION will control the hull domain shader
+	/// </summary>
+	void OnPreRender(){
+		if (cam.actualRenderingPath == RenderingPath.DeferredShading) {
+			Shader.EnableKeyword ("ENABLE_TESSELLATION");
+		} else {
+			Shader.DisableKeyword ("ENABLE_TESSELLATION");
+		}
+	}
+}
